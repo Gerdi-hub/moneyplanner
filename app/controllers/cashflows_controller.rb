d14405637da299657c4ac2f99538dc2b324882a7
Gerdi-hub <orawakene@gmail.com>
2025-01-05 19:25:10 +0200

    swedbank added

--- app/controllers/cashflows_controller.rb
+++ app/controllers/cashflows_controller.rb
@@ -42,7 +42,6 @@ class CashflowsController < ApplicationController
 
   def import
     if params[:file].blank?
-      Rails.logger.debug "File is missing in params"
       return redirect_to root_path, alert: "Please upload a file"
     else
       Rails.logger.debug "File present: #{params[:file].inspect}"
@@ -55,6 +54,10 @@ class CashflowsController < ApplicationController
     if bank == "seb"
       Rails.logger.debug "Processing SEB CSV file"
       process_seb_csv(file)
+    elsif bank == "swedbank"
+        Rails.logger.debug "Processing SWEDBANK CSV file"
+        process_swedbank_csv(file)
+
     else
       Rails.logger.debug "Unsupported bank format: #{bank}"
       redirect_to root_path, alert: "Unsupported bank format"
@@ -87,6 +90,36 @@ class CashflowsController < ApplicationController
         amount *= -1 if debit_credit == "D"
 
         # Save the data to the Cashflow table
+        Cashflow.create!(
+          user: current_user,
+          date: date,
+          description: description,
+          amount: amount
+        )
+      end
+
+      redirect_to root_path, notice: "CSV imported successfully"
+    rescue => e
+      redirect_to root_path, alert: "Error importing CSV: #{e.message}"
+    end
+  end
+
+  def process_swedbank_csv(file)
+    begin
+      csv_options = { col_sep: ";", headers: true, encoding: "r:bom|utf-8" }
+      CSV.foreach(file.path, "r", **csv_options) do |row|
+        next if row.any?(&:nil?) || ["K2", "LS", "AS"].include?(row["Tehingu tüüp"])# Skip rows with nil values
+        Rails.logger.debug "Processing row: #{row.inspect}"
+        if row.empty?
+          Rails.logger.debug "Empty row detected, skipping"
+          next
+        end
+        date = Date.strptime(row["Kuupäev"], "%d.%m.%Y") rescue nil
+        description = [row['Saaja/Maksja'], row['Selgitus']].compact.join(' ')
+        amount = row["Summa"].gsub(",", ".").to_f
+        debit_credit = row["Deebet/Kreedit"]
+        amount *= -1 if debit_credit == "D"
+
         Cashflow.create!(
           user: current_user,
           date: date,
