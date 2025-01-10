35ae8e6c95ade51963562b33031f16b968b6b1a2
Gerdi-hub <orawakene@gmail.com>
2025-01-10 13:56:16 +0200

    Added filter to cashflow page

--- app/controllers/cashflows_controller.rb
+++ app/controllers/cashflows_controller.rb
@@ -3,8 +3,40 @@ class CashflowsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @cashflows = current_user.cashflows.active # Show only active (not soft-deleted) cashflows
-  end
+    @current_user = current_user
+    @user_cashflows = current_user.cashflows.active
+    # Get all available years from cashflow records
+    @available_years = @user_cashflows.distinct.pluck(Arel.sql("strftime('%Y', date)")).sort.reverse
+    @available_months = @user_cashflows.distinct.pluck(Arel.sql("strftime('%m-%Y', date)")).sort.reverse.map { |month| Date.strptime(month, "%m-%Y") }
+
+
+    # If years are selected, filter the data
+    if params[:years].present?
+      @selected_years = params[:years]
+      @cashflows = @user_cashflows.where(Arel.sql("strftime('%Y', date) IN (?)"), @selected_years)
+    else
+      @selected_years = []
+      @cashflows = @user_cashflows.all
+    end
+
+    if params[:months].present?
+      @selected_months = params[:months]
+      @cashflows = @user_cashflows.where(Arel.sql("strftime('%m-%Y', date) IN (?)"),  @selected_months)
+    else
+      @selected_months = []
+      @cashflows = @user_cashflows.all
+    end
+
+    # Group the cashflows by month
+    @monthly_data = @cashflows.group_by do |cashflow|
+      cashflow.date.strftime("%m-%Y")
+    end.transform_values do |cashflows|
+      # Group by type and calculate sums
+      types_data = cashflows.group_by(&:type_name).transform_values do |grouped_cashflows|
+        grouped_cashflows.sum(&:amount)
+      end
+    end
+    end
 
   def new
     @cashflow = Cashflow.new
